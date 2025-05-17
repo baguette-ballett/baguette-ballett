a0bc6f00787327a9b3f4e4b6a1c4b8c03ccc1cc5
Robert <137289566+RobertRenk@users.noreply.github.com>
2025-05-17 20:39:50 +0200

    Use class_names

--- scenes/baguette/baguette.gd
+++ scenes/baguette/baguette.gd
@@ -1,3 +1,4 @@
+class_name Baguette
 extends RigidBody2D
 
 var area_collison = false
@@ -7,17 +8,18 @@ func _ready():
 	self.rotation_degrees = randf_range(-180.0, 180.0)
 
 func _on_body_entered(body):
-	if body.name == "ground body":
+	if body is Ground:
 		get_parent().call_deferred("remove_child", self)
 		self.queue_free()
 		return
 
-	if has_collide or body.name == self.name:
+	if has_collide:
 		return
 
-	var joint = PinJoint2D.new()
-	joint.node_a = body.get_path()
-	joint.node_b = self.get_path()
-	joint.position = self.global_position
-	get_tree().current_scene.add_child(joint)
-	has_collide = true
+	if body is Player or body is Baguette:
+		var joint = PinJoint2D.new()
+		joint.node_a = body.get_path()
+		joint.node_b = self.get_path()
+		joint.position = self.global_position
+		get_tree().current_scene.add_child(joint)
+		has_collide = true
