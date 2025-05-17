662c14a5769e60c8a06e63a5cf5830af1e27b65a
Bernd Stellwag <burned@zerties.org>
2025-05-17 11:14:19 +0200

    attach baguette to player on touch

--- baguette.gd
+++ baguette.gd
@@ -4,4 +4,11 @@ func _on_player_area_body_entered(body):
 	if body.name != "baguette":
 		return
 
-	set_deferred("freeze", true)
+	var player = get_parent().get_node("player")
+	var baguette = body
+
+	var joint1 = PinJoint2D.new()
+	joint1.node_a = player.get_path()
+	joint1.node_b = baguette.get_path()
+	joint1.position = baguette.global_position
+	get_tree().current_scene.add_child(joint1)
