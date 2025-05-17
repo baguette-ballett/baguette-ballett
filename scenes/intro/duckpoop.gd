22c6cba0b08e1ef3c2e01720b168969358ee168a
Thomas Christlieb <thomaschristlieb@hotmail.com>
2025-05-17 16:03:12 +0200

    Resize Images. Try to make poop splash

--- scenes/intro/duckpoop.gd
+++ scenes/intro/duckpoop.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
 func _physics_process(_delta):
-	velocity = Vector2(0, 300)
-	if position.y < 455:
+	velocity = Vector2(0, 500)
+	if position.y < 630:
 		move_and_slide()
-	
+	else:
+		scale.x = 0.5
+		scale.y = 0.5
