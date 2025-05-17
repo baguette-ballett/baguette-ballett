fcc28c49fdbb703167117320ffc1a7b7eb2a3b1b
Robert <137289566+RobertRenk@users.noreply.github.com>
2025-05-17 15:11:28 +0200

    Add Baguette spawner

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+@export var baguettes: PackedScene
+
 func _process(delta):
 	if Input.is_action_pressed("ui_r_key"):
 		get_tree().reload_current_scene()
+
+func _on_spawn_timer_timeout():
+	var baguette = baguettes.instantiate()
+	var location = get_node("SpawnArea/SpawnAreaLocation")
+	location.progress_ratio = randf()
+	
+	baguette.position = location.position
+
+	add_child(baguette)
