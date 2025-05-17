82ddf1053de209354ba4eed42741797b2c602db6
Thomas Christlieb <thomaschristlieb@hotmail.com>
2025-05-17 17:13:52 +0200

    Leave Intro with enter. Make Poop splash.

--- scenes/intro/duckpoop.gd
+++ scenes/intro/duckpoop.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-func _physics_process(_delta):
+func _physics_process(_delta):	
 	velocity = Vector2(0, 500)
-	if position.y < 630:
+	if position.y < 500:
 		move_and_slide()
 	else:
-		scale.x = 0.5
-		scale.y = 0.5
+		get_node("Sprite2D").scale.x = 10
+		get_node("Sprite2D").scale.y = 9
--- scenes/intro/intro.gd
+++ scenes/intro/intro.gd
@@ -1,1 +1,5 @@
 extends Node2D
+
+func _physics_process(_delta):
+	if (Input.is_key_pressed(KEY_ENTER)):
+		get_tree().change_scene_to_file("res://scenes/level/level.tscn")
