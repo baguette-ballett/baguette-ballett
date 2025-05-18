f7cc69faf26622f1442a80b9c76b8c94d5955b89
Bernd Stellwag <burned@zerties.org>
2025-05-18 11:34:37 +0200

    collect baguettes on creation, remove them on destruction, and track if they are in range of the duck

--- scenes/baguette/baguette.gd
+++ scenes/baguette/baguette.gd
@@ -3,20 +3,26 @@ extends RigidBody2D
 
 var area_collison = false
 var has_collide = false
+var isInRange = false
 
 func _ready():
 	self.rotation_degrees = randf_range(-180.0, 180.0)
+	Globals.baguettes.append(self)
 
 func _physics_process(delta):
 	var diffX = abs(self.global_position.x - Globals.player.global_position.x)
 	var diffY = Globals.player.global_position.y - self.global_position.y
 	if diffX < 100 and diffY < 200 and diffY > -10:
+		isInRange = true
 		var direction = (Globals.player.global_position - self.global_position).normalized()
 		direction.y = 0
 		self.apply_impulse(direction*1000)
+	else:
+		isInRange = false
 
 func _on_body_entered(body):
 	if body is Ground:
+		Globals.baguettes.erase(self)
 		get_parent().call_deferred("remove_child", self)
 		self.queue_free()
 		return
--- scenes/globals.gd
+++ scenes/globals.gd
@@ -1,4 +1,4 @@
 extends Node
 
-var jointBaguettes = []
+var baguettes = []
 var player
