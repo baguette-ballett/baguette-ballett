831024911be89d5b7208252e21e30f166658102d
atmidian <git@dposs.de>
2025-05-18 12:01:01 +0200

    Add Area for pond, and logic for pond entered/exited
     arrow down to drop bred

--- scenes/globals.gd
+++ scenes/globals.gd
@@ -3,5 +3,13 @@ extends Node
 var baguettes = []
 var player
 
+var playerOverPond = false
+
 func getBaguettesInRange():
 	return baguettes.filter(func(baguette): return baguette.isInRange)
+
+
+func setPlayrOverPond(value) ->void:
+	playerOverPond = value
+	print(playerOverPond)
+
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -17,5 +17,17 @@ func get_input(delta):
 		velocity.x = lerp(velocity.x, walkSpeed, acceleration)
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = lerp(velocity.x, walkSpeed*-1.0, acceleration)
+	elif Input.is_action_just_pressed("ui_down"):
+		print("Drop Bread")
 	else:
 		velocity.x = lerp(velocity.x, 0.0, 0.1)
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if !(body is Player):
+		return
+	Globals.setPlayrOverPond(true)
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if !(body is Player):
+		return
+	Globals.setPlayrOverPond(false)
